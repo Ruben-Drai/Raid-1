3b262bdd493d9545a8cf653189def4d555449f28
A-Clivet <a.clivet@lahorde.school>
2023-06-12 17:28:24 +0200

    Script Maj

--- Assets/Scripts/Interactibles/SparePart.cs
+++ Assets/Scripts/Interactibles/SparePart.cs
@@ -1,10 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
 public class SparePart : Interactible
 {
+    [SerializeField] Light2D highlight;
     public override void Interact()
     {
         IsActivated = true;
         PlayerController.UnlockedUpgrades[name] = true;
         PlayerController.instance.AvailableInteraction = null;
         gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            highlight.intensity = 1.39f;
+        }
+    }
+    private void OnTriggerExit2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            highlight.intensity = 0;
+        }
     }
 }
