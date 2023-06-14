723a916b3e07595814ca84aea8e7b93e8ee6b633
DraiRuben <r.drai@lahorde.school>
2023-06-14 19:20:26 +0200

    bug fixes

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -30,7 +30,7 @@ public class MainMenu : MonoBehaviour
     {
         //TODO: Check for save, if no save, don't make button available to click.
         SceneManager.LoadSceneAsync(PlayerPrefs.GetString("SceneName"));
-        SaveNLoad.instance.StartCoroutine(SaveNLoad.instance.LoadRoutine(true));
+        SaveNLoad.instance.StartCoroutine(SaveNLoad.instance.LoadRoutine(false));
         Time.timeScale = 1;
         SoundManager.instance.Click.PlayOneShot(SoundManager.instance.Click.clip);
 
